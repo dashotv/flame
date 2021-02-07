b642f7e26661f3ad40c7255e47ba17aeb0682483
Shawn Catanzarite <me@shawncatz.com>
2021-02-06 22:05:17 -0800

    bump 0.5.8

--- ruby/lib/flame/version.rb
+++ ruby/lib/flame/version.rb
@@ -2,7 +2,7 @@ module Flame
   module Version
     MAJOR  = 0
     MINOR  = 5
-    TINY   = 7
+    TINY   = 8
     TAG    = nil
     LIST   = [MAJOR, MINOR, TINY, TAG]
     STRING = LIST.compact.join('.')

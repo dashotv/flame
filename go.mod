c6070925e908af714e16e51ec174070c9360b229
Shawn Catanzarite <me@shawncatz.com>
2024-06-04 03:25:45 +0000

    fix: use circuit breaker for qbt communications, should prevent issues when it restarts

--- go.mod
+++ go.mod
@@ -136,6 +136,8 @@ require (
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/schollz/closestmatch v2.1.0+incompatible // indirect
+	github.com/sony/gobreaker v1.0.0 // indirect
+	github.com/sony/gobreaker/v2 v2.0.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
--- internal/app/workers_updates.go
+++ internal/app/workers_updates.go
@@ -5,15 +5,46 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"github.com/sony/gobreaker/v2"
 
 	"github.com/dashotv/flame/nzbget"
 	"github.com/dashotv/flame/qbt"
 )
 
+var qbtCircuitBreaker *gobreaker.CircuitBreaker[*qbt.Response]
+
+func init() {
+	initializers = append(initializers, setupUpdates)
+}
+
+func setupUpdates(a *Application) error {
+	qbtCircuitBreaker = gobreaker.NewCircuitBreaker[*qbt.Response](gobreaker.Settings{
+		Name: "qbt",
+		// ReadyToTrip: func(counts gobreaker.Counts) bool {
+		// 	return counts.ConsecutiveFailures > 3
+		// },
+		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
+			a.Workers.Log.Infof("qbt: state change: %s -> %s", from, to)
+			if to == gobreaker.StateHalfOpen {
+				if err := setupQbittorrent(a); err != nil {
+					a.Workers.Log.Errorf("qbt: re-init failed: %s", err)
+				}
+			}
+		},
+		Timeout: 5 * time.Second,
+	})
+	return nil
+}
+
 func Updates() error {
 	// app.Log.Named("updates").Info("starting")
-	qbt, err := app.Qbt.List()
+	qr, err := qbtCircuitBreaker.Execute(func() (*qbt.Response, error) {
+		return app.Qbt.List()
+	})
 	if err != nil {
+		if errors.Cause(err) == gobreaker.ErrOpenState {
+			return nil
+		}
 		return errors.Wrap(err, "getting torrent list")
 	}
 
@@ -24,23 +55,23 @@ func Updates() error {
 
 	// check disk space every minute
 	if time.Now().Unix()%60 == 0 {
-		go checkDisk(nzbs.Status.FreeDiskSpaceMB, qbt.AllPaused())
+		go checkDisk(nzbs.Status.FreeDiskSpaceMB, qr.AllPaused())
 	}
 
-	app.Events.Send("flame.qbittorrents", qbt)
+	app.Events.Send("flame.qbittorrents", qr)
 	app.Events.Send("flame.nzbs", nzbs)
 
-	go updateQbittorrents(qbt)
+	go updateQbittorrents(qr)
 	go updateNzbs(nzbs)
 	go func() {
 		metrics := &Metrics{}
 		metrics.Diskspace = fmt.Sprintf("%2.1f", float64(nzbs.Status.FreeDiskSpaceMB/1000))
-		metrics.Torrents.DownloadRate = fmt.Sprintf("%2.1f", float64(qbt.DownloadRate/1000))
-		metrics.Torrents.UploadRate = fmt.Sprintf("%2.1f", float64(qbt.UploadRate/1000))
+		metrics.Torrents.DownloadRate = fmt.Sprintf("%2.1f", float64(qr.DownloadRate/1000))
+		metrics.Torrents.UploadRate = fmt.Sprintf("%2.1f", float64(qr.UploadRate/1000))
 		metrics.Nzbs.DownloadRate = fmt.Sprintf("%2.1f", float64(nzbs.Status.DownloadRate/1000))
 		app.Events.Send("flame.metrics", metrics)
 		app.Events.Send("flame.combined", &Combined{
-			Torrents:  qbt.Torrents,
+			Torrents:  qr.Torrents,
 			Nzbs:      nzbs.Result,
 			NzbStatus: nzbs.Status,
 			Metrics:   metrics,
